5d4555edffec1730fc9a907832ff111e7c402c4c
HongzCloud <jihong21511@gmail.com>
2020-12-08 00:57:40 +0900

    ✅ Add step-1 TestCode

---
+++ RubiksCube/RubiksCube/Step-1.swift
@@ -0,0 +1,12 @@
+//
+//  Step-1.swift
+//  RubiksCube
+//
+//  Created by 오킹 on 2020/12/08.
+//
+
+import Foundation
+
+func pushWord(_ input: String) -> String {
+    return ""
+}
--- RubiksCube/RubiksCube/main.swift
+++ RubiksCube/RubiksCube/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK:- Step1 Test Code
+func testStep1() {
+    print(pushWord("apple 3 L") == "leapp" ? "PASS":"FAILED")
+    print(pushWord("banana 6 R") == "banana" ? "PASS":"FAILED")
+    print(pushWord("carrot -1 r") == "arrotc" ? "PASS":"FAILED")
+    print(pushWord("cat -4 R") == "atc" ? "PASS":"FAILED")
+}
+testStep1()
 
+//User input Test Code
+//let input = readLine() ?? ""
+//pushWord(input)
