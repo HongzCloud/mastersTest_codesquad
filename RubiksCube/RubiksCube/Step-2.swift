e5933ade4181ce9b9c9ebf8e43083a0064f28db9
HongzCloud <jihong21511@gmail.com>
2020-12-09 20:06:19 +0900

    ✨ Create Step-2.swift file and Add startGame function

---
+++ RubiksCube/RubiksCube/Step-2.swift
@@ -0,0 +1,17 @@
+//
+//  Step-2.swift
+//  RubiksCube
+//
+//  Created by 오킹 on 2020/12/09.
+//
+
+import Foundation
+
+struct FlatCubeManager {
+    func startGame() {
+        //초기셋팅
+        //사용자 조작 입력
+        //큐브 조작
+        //결과
+    }
+}
--- RubiksCube/RubiksCube/main.swift
+++ RubiksCube/RubiksCube/main.swift
@@ -7,5 +7,7 @@
 
 import Foundation
 
-print("Hello, World!")
-
+func testStep2() {
+   let flatCubeManager = FlatCubeManager()
+    flatCubeManager.startGame()
+}
