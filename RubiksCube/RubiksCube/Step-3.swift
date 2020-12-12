0629077e589457ce19f9e9dfc24751705d61044c
HongzCloud <jihong21511@gmail.com>
2020-12-13 00:36:57 +0900

    ✨ Add pushForL() for 'L' case of push function

--- RubiksCube/RubiksCube/Step-3.swift
+++ RubiksCube/RubiksCube/Step-3.swift
@@ -103,7 +103,19 @@ struct RubiksCubeManager {
     
     func pushForL(_ cube: [[[Character]]]) -> [[[Character]]] {
         var myCube = cube
-
+        var tempValue = [Character]()
+        var tempValue2 = [Character]()
+ 
+        for row in 0..<3 {
+            tempValue.append(myCube[4][row][2])
+            tempValue2.append(myCube[5][row][2])
+            
+            myCube[5][row][0] = myCube[1][row][0]
+            myCube[4][row][0] = myCube[3][2-row][2]
+            myCube[3][2-row][2] = tempValue2[row]
+            myCube[1][row][0] = tempValue[row]
+        }
+        
         return myCube
     }
     
